7db81a62c164bcf8d81349a1552b1a25a256dc0c
Michel Kim <kim98.michel@gmail.com>
2023-04-27 23:05:27 -0300

    configure bottom navigation

--- app/src/main/java/br/com/manga/ui/main/MainFragment.kt
+++ app/src/main/java/br/com/manga/ui/main/MainFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import br.com.manga.R
 import br.com.manga.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
@@ -17,5 +19,46 @@ class MainFragment : Fragment() {
     ): View {
         binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setViews()
+    }
+
+    private fun setViews() = with(binding) {
+        bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.bnHome -> {
+                    homeFragment.root.isVisible = true
+                    mangaFragment.root.isVisible = false
+                    searchFragment.root.isVisible = false
+                    profileFragment.root.isVisible = false
+                    true
+                }
+                R.id.bnManga -> {
+                    homeFragment.root.isVisible = false
+                    mangaFragment.root.isVisible = true
+                    searchFragment.root.isVisible = false
+                    profileFragment.root.isVisible = false
+                    true
+                }
+                R.id.bnSearch -> {
+                    homeFragment.root.isVisible = false
+                    mangaFragment.root.isVisible = false
+                    searchFragment.root.isVisible = true
+                    profileFragment.root.isVisible = false
+                    true
+                }
+                R.id.bnProfile -> {
+                    homeFragment.root.isVisible = false
+                    mangaFragment.root.isVisible = false
+                    searchFragment.root.isVisible = false
+                    profileFragment.root.isVisible = true
+                    true
+                }
+                else -> {true}
+            }
+        }
     }
 }
